e60910c5c54284f19dd3468cf5a7bf1c4fd4bb4b
natt001og <natalia.ortega1@mail.udp.cl>
2025-05-05 15:21:43 -0400

    adición de comentarios +'logica para conectar fbase

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,15 @@
-const express = require('express');
+const express = require('express'); //importamos express
 
 const app = express();
+const morgan = require('morgan');   //esto es solo para ver las consultas en la consola
 
-module.exports = app;
+app.use(morgan('dev')); 
+
+const {db} = require('./firebase'); // para traer el objeto db que se exporta de firebase.js
+
+app.get('/', async (req, res) => {
+    const querySnapshot = await db.collection('users').get()    //consulta de prueba a db
+
+    console.log(querySnapshot.docs[0].data()); //para ver los datos que se recuperan
+})
+module.exports = app;  //esporta app para usarlo en index.js
---
+++ backend/firebase.js
@@ -0,0 +1,16 @@
+//Aquí está la lógica para conectarnos a Firebase
+
+require('dotenv').config(); //solo para usar los .env
+const{initializeApp, applicationDefault}=require('firebase-admin/app') ;//para inicializar firebase en el proyecto 
+const{getFirestore}=require('firebase-admin/firestore'); //para acceder al servicio firestore (a la bbdd en si)
+
+initializeApp({
+    credential: applicationDefault(), //busca automaticamente las credenciales, para esto deben 
+                                     //dentro del archivo .env dentro de la carpeta backend
+});
+
+const db = getFirestore();  //obtiene la db
+
+module.exports = {
+    db    //la exportamos para usarla en app.js
+};
