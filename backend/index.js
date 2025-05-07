3dbb65d406ff91e45e8213b958a1b14ad11ca540
HugoosZ <hugo.rojas1@mail.udp.cl>
2025-05-06 20:14:01 -0400

    Prueba cambios en index y vercel

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,15 @@ const app= require('./app.js')  //recupera lo que se exporta en app
 app.listen(3000)
 console.log('server is running on port 3000') //para mostrar lo que muestra app
 
+
+app.get('/', (req, res) => {
+    res.send('Prueba de que funciona');
+  });
+  
+  // Configuración del puerto para Vercel
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  }).on('error', (err) => {
+    console.error('Server error:', err);
+  });
