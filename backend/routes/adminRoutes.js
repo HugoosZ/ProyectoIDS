5dc187c6bd08304ec3c1b464fe3532a0df946ea2
HugoosZ <hugo.rojas1@mail.udp.cl>
2025-05-10 19:49:44 -0400

    Arreglo de rutas con autentificacion

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,8 +6,17 @@ const userController = require('../controllers/userController');
 const { checkAdminPrivileges } = require('../middlewares/authorization'); // Middleware de autorización
 // Se usa llaves en la asignacion de nombres para poder renombrarlos, si no, hay que ponerle el nombre del codigo y como son parecidos es mejor renombrar
 
-// router.post('/createUser', verifyAndDecodeToken, checkAdminPrivileges, userController.createUser); !!!!
-router.post('/createUser', userController.createUser);
+
+
+router.get('/checkAdmin', verifyAndDecodeToken, checkAdminPrivileges, async (req, res) => {
+    // Como ya se pasaron las auntenticaciones se puede postear el json
+    res.json({ isAdmin: true }); 
+    
+});
+
+router.post('/createUser', verifyAndDecodeToken, checkAdminPrivileges, userController.createUser); 
+
+
 router.get('/', async (req, res) => {
     res.send("¡Ruta /api/admin funciona correctamente!");
 });
