be8c7cdead9e0dd377e8306799e8b03042071fc8
Thomas <thomas.munoz1@mail.udp.cl>
2025-05-26 19:53:16 -0400

    Tasky subido a develop sin backend

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-const app= require('./app.js')  //recupera lo que se exporta en app
-//import app from './app.js'; // 👈 Extensión .js obligatoria
-
-app.listen(3000)
-console.log('server is running on port 3000') //para mostrar lo que muestra app
