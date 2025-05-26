643384ffc8082006855d3059fbf8f2a5b8a14c76
vertiass <tomas.leon1@mail.udp.cl>
2025-05-26 18:18:12 -0400

    adicion de GET de tareas en middlewares

---
+++ backend/middlewares/retrieve_tasks.js
@@ -0,0 +1,33 @@
+exports.getAllTasks = async (req, res) => {
+  try {
+    const snapshot = await db.collection("tasks").get();
+    const tasks = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error al obtener tareas:", error);
+    res.status(500).json({ error: "Error al obtener tareas" });
+  }
+};
+
+exports.getUserTasks = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const snapshot = await db
+      .collection("tasks")
+      .where("assignedTo", "==", userId)
+      .get();
+    const tasks = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Error al obtener tareas:", error);
+    res.status(500).json({ error: "Error al obtener tareas" });
+  }
+};
