d9217df2f9eaa1fc48d9951b2f3dadd16001a1fe
HugoosZ <132111633+HugoosZ@users.noreply.github.com>
2025-05-26 16:59:28 -0400

    Eliminacion carpetas y rutas sin usar

--- backend/app.js
+++ backend/app.js
@@ -24,21 +24,14 @@ const mainRouter = require('./routes/index');
 const adminRouter = require('./routes/adminRoutes');
 const taskRoutes = require('./routes/tasks');
 const usersRoutes = require('./routes/users');
-//const aux = require('./routes/test.js')
-const authRoutes = require('./routes/authRoutes');
-const emailRoutes = require('./routes/emailRoutes');
 
 //Rutas publicas
 app.use('/api', taskRoutes);
 app.use('/api', usersRoutes);
-//app.use('/api', aux);
 app.use('/api', mainRouter);       
-app.use('/api/email', emailRoutes); // Nuevas rutas de correo electrónico
-
 
  // Rutas admin!!!
 app.use('/api/', adminRouter); // Rutas admin empezarán con /api/admin
-app.use('/api/auth', authRoutes); //De prueba!! Se puede borrar
 
 
 module.exports = app;  //esporta app para usarlo en index.js
